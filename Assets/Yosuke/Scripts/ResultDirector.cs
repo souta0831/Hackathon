e42f769f69c84be316af581dd91f4b26874a502b
magumaron <magumaron1@gmail.com>
2019-08-17 18:59:14 +0900

    SEの追加 BGMの追加 リザルト関連

--- Assets/Yosuke/Scripts/ResultDirector.cs
+++ Assets/Yosuke/Scripts/ResultDirector.cs
@@ -9,30 +9,79 @@ public class ResultDirector : MonoBehaviour
 
     public GameObject titleButton;
     public GameObject continueButton;
+    public GameObject panel;
+    public GameObject panel2;
+    public GameObject moveText;
+    public GameObject mileageText;
+    public AudioSource gameBgm;
+
+    public AudioClip gameoverSE;
+    public AudioClip tapSE;
+
+    public float speed = 0.005f;
+    float alfa;
+    float red, green, blue;
+    int fadeout_frag;
+    AudioSource audioSource1;
+    AudioSource audioSource2;
 
     void Start()
     {
+   
+        red = panel.GetComponent<Image>().color.r;
+        green = panel.GetComponent<Image>().color.g;
+        blue = panel.GetComponent<Image>().color.b;
+
+        fadeout_frag = 0;
+
         titleButton.SetActive(false);
         continueButton.SetActive(false);
+        panel2.SetActive(false);
+        moveText.SetActive(false);
 
+        audioSource1 = GetComponent<AudioSource>();
+        audioSource2 = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        //倒れたときの処理
         if (Input.GetMouseButtonDown(0))
         {
             titleButton.SetActive(true);
             continueButton.SetActive(true);
+            panel2.SetActive(true);
+            moveText.SetActive(true);
+            gameBgm.Stop();
+
+            audioSource1.PlayOneShot(gameoverSE);
+
+            fadeout_frag = 1;
+        }
+
+        if (fadeout_frag == 1)
+        {
+            alfa += speed;
+
+            if (alfa >= 0.5)
+            {
+                speed = 0;
+            }
+
+            panel.GetComponent<Image>().color = new Color(red, green, blue, alfa);
         }
     }
 
+
     public void OpButtonClicked()
     {
+        audioSource2.PlayOneShot(tapSE);
         SceneManager.LoadScene("OP");
     }
 
     public void ContinueButtonClicked()
     {
+        audioSource2.PlayOneShot(tapSE);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
