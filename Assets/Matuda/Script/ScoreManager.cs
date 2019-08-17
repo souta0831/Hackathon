243590887e83d560b12b48c4926878b9dad3325c
souta_notePC <darkwind0831@gmail.com>
2019-08-17 18:19:08 +0900

    スコアクラスと素材ファイルにフォントを追加

---
+++ Assets/Matuda/Script/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+ class ScoreManager : MonoBehaviour
+{
+    public float _now_score=0;
+    [SerializeField]
+    private Text DrawText;
+    [SerializeField]
+   private PedalManager Pedal;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _now_score += Pedal.GetNowSpeed()/100 ;
+        DrawText.text = "スコア:" + (int)_now_score;
+    }
+}
